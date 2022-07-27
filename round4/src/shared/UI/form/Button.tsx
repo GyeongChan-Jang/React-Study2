e7276acc837f27d6b95fc6b51c9d1832ad28a19b
chan <cham9994@gmail.com>
2022-07-28 00:31:21 +0900

    ✨ round4 init: firebase 로그인 연동

---
+++ round4/src/shared/UI/form/Button.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { ButtonTypes } from '../../enum'
+
+interface ChildProps {
+  href?: string
+  children?: React.ReactNode
+  large?: boolean
+  small?: boolean
+  danger?: boolean
+  inverse?: boolean
+  defaultColor?: boolean
+  to?: string
+  disabled?: boolean
+  type?: ButtonTypes
+  defaultSize?: string
+  toggleSignInAndSignUp?: any
+  onSubmit?: any
+  onDarkMode?: any
+}
+
+const Button = ({
+  href,
+  children,
+  large,
+  small,
+  danger,
+  inverse,
+  type,
+  defaultColor,
+  to,
+  disabled,
+  toggleSignInAndSignUp,
+  onSubmit,
+  onDarkMode
+}: ChildProps) => {
+  return (
+    <button
+      className={`px-4 py-2 rounded text-white inline-block shadow-lg bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 ${
+        large ? 'w-64' : ''
+      } ${small ? 'w-28 h-10 text-xs break-normal' : ''}`}
+      type={type}
+      disabled={disabled}
+      onClick={toggleSignInAndSignUp || onDarkMode}
+      onSubmit={onSubmit}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
